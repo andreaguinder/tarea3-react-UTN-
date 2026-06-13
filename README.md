a4c6ab74ba8f93e317027fc83ebd98c1d1f8e2e8
Andy Guinder <andreabelen.guinder@gmail.com>
2026-06-13 19:13:19 -0300

    tarea 3 javascript avanzado gestor de tareas

---
+++ README.md
@@ -0,0 +1,34 @@
+# Curso de Desarrollo en React JS - Andrea Belén Guinder Vichich - Módulo 1 - Unidad 2 - Tarea N2
+
+## ℹ️ Información sobre el proyecto
+
+### Descripción
+
+La tarea se basa en un Gestor de tareas. El trabajo se realizó todo en JavaScript para ejecutarse en la consola. Se definieron dos clases, una con un constructor y un método, y otra con un array y cuatro métodos.
+Se realizó una simulación asincrónica con una función que devuelve una Promesa en cuyo resolve se devuelve un array con las tres tareas iniciales de la lista.
+Se puede agregar, listar o buscar las tareas de acuerdo a lo pedido en la consigna y se utiliza async/await para la ejecución del programa. Cada paso que realiza el programa es mostrado por consola.
+
+## 🚀 Instrucciones de Instalación y Ejecución
+
+Para clonar este proyecto y visualizarlo de forma local en tu navegador, seguí estos pasos:
+
+### 1. Obtener la URL del repositorio
+1. Entrá al repositorio en GitHub.
+2. Hacé clic en el botón verde que dice **Code** (ubicado arriba a la derecha).
+3. Asegurate de seleccionar la pestaña **HTTPS** y copiá la URL que te aparece.
+
+### 2. Clonar el repositorio
+Abrí tu terminal o consola de comandos, posicionate en la carpeta donde quieras guardar el proyecto y ejecutá el siguiente comando (reemplazando por la URL que copiaste):
+
+### 3. En la Terminal del Editor de código
+git clone [https://github.com/andreaguinder/tarea2-react-UTN.git](https://github.com/andreaguinder/tarea2-react-UTN.git)
+
+### 4. En el Editor de código
+Una vez que tenés el proyecto en tu editor, con la extensión Live Server desde el HTML lo abrís en el navegador y luego con F12 abrís la consola para inspeccionar. En "console" aparecerá toda la ejecución del programa.
+
+## 📸 Captura de pantalla de la página final
+
+* Dentro de la carpeta images adjunto capturas de la consola.
+
+
+## 🔗 [Portfolio Andrea Guinder](https://andreaguinder.com/)
---
+++ js/app.js
@@ -0,0 +1,84 @@
+//1
+class Tarea {
+    constructor(id, titulo, completada = false) {
+        this.id = id;
+        this.titulo = titulo;
+        this.completada = completada;
+    }
+
+    toggleEstado() {
+        return this.completada = !this.completada;
+    }
+}
+
+class GestorTareas {
+
+    arrayDeTareas = [];
+
+    agregarTareas(titulo) {
+        const nuevaTarea = this.arrayDeTareas.length + 1;
+        this.arrayDeTareas.push(new Tarea(nuevaTarea, titulo));
+    }
+
+    listarTareas() {
+        this.arrayDeTareas.forEach(tarea => console.log(tarea));
+    }
+
+    buscarPorTitulo(titulo) {
+        return this.arrayDeTareas.find(tarea => tarea.titulo.toLowerCase() === titulo.toLowerCase());
+    }
+
+    listarCompletadas() {
+        return this.arrayDeTareas.filter(tarea => tarea.completada);
+    }
+}
+
+
+//2
+function cargarTareas() {
+    return new Promise((resolve) => {
+
+        setTimeout(() => {
+            const tareasIniciales = [
+                new Tarea(1, "Sacar la basura", true),
+                new Tarea(2, "Lavar la ropa"),
+                new Tarea(3, "Tender la cama")
+            ];
+            resolve(tareasIniciales);
+        }, 2000);
+
+    });
+}
+
+//3
+async function ejecutarPrograma() {
+
+    const gestor = new GestorTareas();
+
+    console.log("Cargando tareas...");
+
+    const tareasCargadas = await cargarTareas();
+
+
+    gestor.arrayDeTareas = tareasCargadas;
+    console.log("Tareas cargadas correctamente.");
+
+    console.log("\n--- Lista Inicial ---");
+    gestor.listarTareas();
+
+    console.log("\n--- Agregando nueva tarea ---");
+    gestor.agregarTareas("Ir al super");
+    gestor.listarTareas();
+
+    console.log("\n--- Tareas Completadas ---");
+    const completadas = gestor.listarCompletadas();
+    completadas.forEach(tarea => console.log(tarea));
+
+//4
+    console.log("\n--- Extra: Solo los títulos ---");
+    const nuevoArrayDeTareas = gestor.arrayDeTareas.map(tarea => tarea.titulo);
+    console.log(nuevoArrayDeTareas);
+}
+
+
+ejecutarPrograma();
